658b889ae382fc75dc116a6427f92cbfa0b8f8ba
xplshn <114888778+xplshn@users.noreply.github.com>
2025-06-26 09:32:26 -0300

    Update mpv-appimage.sh

--- mpv-appimage.sh
+++ mpv-appimage.sh
@@ -97,7 +97,7 @@ chmod +x ./pelf
 
 echo "Generating [dwfs]AppBundle...(Go runtime)"
 ./pelf --add-appdir ./AppDir \
-	--appbundle-id="io.mpv.Mpv#github.com/github.com/$GITHUB_REPOSITORY:$VERSION@$(date +%d_%m_%Y)" \
+	--appbundle-id="io.mpv.Mpv#github.com/$GITHUB_REPOSITORY:$VERSION@$(date +%d_%m_%Y)" \
 	--compression "-C zstd:level=22 -S26 -B8" \
 	--output-to mpv-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
 
