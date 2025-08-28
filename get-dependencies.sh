373affdaf86e5c5cb5c93cc3a85e1f9bcfe64e12
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-28 14:37:22 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -48,7 +48,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa --prefer-nano ffmpeg-mini libxml2-mini opus-mini
+./get-debloated-pkgs.sh --add-mesa --prefer-nano libxml2-mini opus-mini
 
 echo "Building mpv..."
 echo "---------------------------------------------------------------"
