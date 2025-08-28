214dc1ed4aee19b637af937fa3c8ba3aa32c8857
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-28 10:18:25 -0400

    simplify deps install,  build mpv here

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,88 +1,69 @@
 #!/bin/sh
 
 set -ex
-
-sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
-
-if [ "$(uname -m)" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
-else
-	PKG_TYPE='aarch64.pkg.tar.xz'
-fi
-LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
-FFMPEG_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/ffmpeg-mini-$PKG_TYPE"
-OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-  alsa-lib \
-  base-devel \
-  desktop-file-utils \
-  ffmpeg \
-  git \
-  glibc \
-  hicolor-icon-theme \
-  jack \
-  lcms2 \
-  libarchive \
-  libass \
-  libbluray \
-  libcdio \
-  libcdio-paranoia \
-  libdrm \
-  libdvdnav \
-  libdvdread \
-  libegl \
-  libgl \
-  libglvnd \
-  libjpeg-turbo \
-  libplacebo \
-  libpulse \
-  libsixel \
-  libva \
-  libvdpau \
-  libx11 \
-  libxext \
-  libxkbcommon \
-  libxpresent \
-  libxrandr \
-  libxss \
-  libxv \
-  luajit \
-  mesa \
-  meson \
-  nasm \
-  patchelf \
-  libpipewire \
-  rubberband \
-  openal \
-  uchardet \
-  vulkan-headers \
-  vulkan-icd-loader \
-  wayland \
-  wayland-protocols \
-  wget \
-  xorg-server-xvfb \
-  zlib \
-  zsync
+	base-devel        \
+	git               \
+	jack              \
+	lcms2             \
+	libarchive        \
+	libass            \
+	libbluray         \
+	libcdio           \
+	libcdio-paranoia  \
+	libdrm            \
+	libdvdnav         \
+	libdvdread        \
+	libjpeg-turbo     \
+	libplacebo        \
+	libpulse          \
+	libsixel          \
+	libx11            \
+	libxext           \
+	libxkbcommon      \
+	libxpresent       \
+	libxrandr         \
+	libxss            \
+	libxv             \
+	luajit            \
+	meson             \
+	nasm              \
+	pipewire-audio    \
+	pulseaudio        \
+	pulseaudio-alsa   \
+	rubberband        \
+	openal            \
+	uchardet          \
+	vulkan-headers    \
+	vulkan-icd-loader \
+	wayland           \
+	wayland-protocols \
+	wget              \
+	xorg-server-xvfb  \
+	zlib              \
+	zsync
 
-#if [ "$(uname -m)" = 'x86_64' ]; then
-#	pacman -Syu --noconfirm vulkan-intel haskell-gnutls gcc13 svt-av1
-#else
-#	pacman -Syu --noconfirm vulkan-freedreno vulkan-panfrost
-#fi
-
-echo "Installing debloated pckages..."
+echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2-iculess.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$FFMPEG_URL" -O ./ffmpeg-mini.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$OPUS_URL" -O ./opus-nano.pkg.tar.zst
-pacman -U --noconfirm ./*.pkg.tar.zst
-rm -f ./*.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-mesa --prefer-nano ffmpeg-mini libxml2-mini opus-mini
 
-# Remove vapoursynth since ffmpeg-mini doesn't link to it
-pacman -Rsndd --noconfirm vapoursynth
+pacman -Rsndd --noconfirm vapoursynth # ffmpeg-mini doesn't link to it
 
-echo "All done!"
+echo "Building mpv..."
 echo "---------------------------------------------------------------"
+git clone "https://github.com/mpv-player/mpv-build.git" ./mpv-build && (
+	cd ./mpv-build
+	printf "%s\n" "--enable-libdav1d" >> ffmpeg_options
+	printf "%s\n" "--enable-small" >> ffmpeg_options
+	printf "%s\n" "-Dlibmpv=false" >> mpv_options
+	./use-mpv-release
+	./rebuild -j$(nproc)
+	sudo ./install
+)
+rm -rf ./mpv-build
