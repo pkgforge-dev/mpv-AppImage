97c688b447943e53c7c0352a45dff6a1a4cd0737
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-18 20:23:40 -0400

    fix CI and update to template (#35)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-set -ex
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+set -eu
 
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel        \
 	dav1d             \
-	git               \
 	lame              \
 	lcms2             \
 	libarchive        \
@@ -22,25 +19,17 @@ pacman -Syu --noconfirm \
 	libjpeg-turbo     \
 	libogg            \
 	libplacebo        \
-	libpulse          \
 	libsixel          \
 	libx11            \
 	libxext           \
-	libxkbcommon      \
-	libxpresent       \
-	libxrandr         \
-	libxss            \
 	libxv             \
 	luajit            \
 	meson             \
 	nasm              \
-	pulseaudio        \
-	pulseaudio-alsa   \
 	rubberband        \
 	openal            \
 	uchardet          \
 	vulkan-headers    \
-	vulkan-icd-loader \
 	wayland           \
 	wayland-protocols \
 	wget              \
@@ -50,17 +39,15 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano
 
 echo "Building mpv..."
 echo "---------------------------------------------------------------"
 git clone "https://github.com/mpv-player/mpv-build.git" ./mpv-build 
-
 cd ./mpv-build
 echo "--enable-libdav1d"      >> ./ffmpeg_options
 echo "--enable-small"         >> ./ffmpeg_options
+echo "--enable-libshaderc"    >> ./ffmpeg_options
 echo "-Dlibmpv=false"         >> ./mpv_options
 echo "-Dlibbluray=disabled"   >> ./mpv_options
 echo "-Dvapoursynth=disabled" >> ./mpv_options
