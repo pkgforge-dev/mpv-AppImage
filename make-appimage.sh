29676263efa516e0515b7a925207f2c18323359c
Samueru-sama <xdglawyer@outlook.com>
2026-04-03 08:25:33 -0400

    use vid file to test since yt-dlp gets blocked in github

--- make-appimage.sh
+++ make-appimage.sh
@@ -29,4 +29,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage https://www.youtube.com/watch?v=YE7VzlLtp-4
+quick-sharun --test ./dist/*.AppImage ./test-video.mp4
