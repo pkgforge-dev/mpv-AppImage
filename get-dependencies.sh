8718a85043b18277e7684f626a7f6ae25b3291aa
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-11 11:42:14 -0400

    fix openssl build

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -64,6 +64,7 @@ echo "--enable-libdav1d"      >> ./ffmpeg_options
 echo "--enable-small"         >> ./ffmpeg_options
 echo "--disable-gnutls"       >> ./ffmpeg_options
 echo "--enable-openssl"       >> ./ffmpeg_options
+echo "--enable-version3"      >> ./ffmpeg_options
 echo "--disable-librsvg"      >> ./ffmpeg_options
 echo "-Dlibmpv=false"         >> ./mpv_options
 echo "-Dlibbluray=disabled"   >> ./mpv_options
